7b72bbcc5a85772d63fa2d89be5ce089a2dd9cb2
Mike Churvis <mikechurvis@gmail.com>
2023-03-23 19:25:02 +0000

    Work continues on core logic

--- api/main.py
+++ api/main.py
@@ -1,274 +1,171 @@
-from __future__ import annotations
-from dataclasses import dataclass
 from enum import Enum
 from typing import Literal
+from pydantic import BaseModel
 
 
-CardLevel = Literal[1, 2, 3]
-
-
-@dataclass
-class GemValue:
-    white: int = 0
-    blue: int = 0
-    green: int = 0
-    red: int = 0
-    black: int = 0
-
-    def __add__(self, other) -> GemValue:
-        if not isinstance(other, GemValue):
-            raise TypeError("other value must be a GemValue.")
-
-        return GemValue(
-            white=self.white + other.white,
-            blue=self.blue + other.blue,
-            green=self.green + other.green,
-            red=self.red + other.red,
-            black=self.black + other.black,
-        )
-
-    def __sub__(self, other) -> GemValue:
-        if not isinstance(other, GemValue):
-            raise TypeError("other value must be a GemValue.")
-
-        return GemValue(
-            white=max(self.white - other.white, 0),
-            blue=max(self.blue - other.blue, 0),
-            green=max(self.green - other.green, 0),
-            red=max(self.red - other.red, 0),
-            black=max(self.black - other.black, 0),
-        )
-
-    def total_value(self) -> int:
-        return self.white + self.blue + self.green + self.red + self.black
-
-
-class GemColor(str, Enum):
-    BLACK = "black"
-    RED = "red"
-    GREEN = "green"
-    BLUE = "blue"
+class Color(str, Enum):
     WHITE = "white"
+    BLUE = "blue"
+    GREEN = "green"
+    RED = "red"
+    BLACK = "black"
+    GOLD = "gold"
 
 
-class PurchasableCardLocation(int, Enum):
-    FIRST_COLUMN = 0
-    SECOND_COLUMN = 1
-    THIRD_COLUMN = 2
-    FOURTH_COLUMN = 3
-    TOP_OF_DECK = 4
+GEM_COLORS = {Color.WHITE, Color.BLUE, Color.GREEN, Color.RED, Color.BLACK}
+TOKEN_COLORS = {*GEM_COLORS, Color.GOLD}
 
 
-TokenColor = GemColor | Literal["gold"]
+class PlayerNecessity(str, Enum):
+    PERFORM_TURN_ACTION = "act"
+    DISCARD_EXCESS_TOKENS = "discard_tokens"
+    SELECT_A_NOBLE = "pick_noble"
+    END_TURN = "end_turn"
 
 
-@dataclass
-class Noble:
-    id: str
-    cost: dict[GemColor, int]
+class TokenInventory(BaseModel):
+    def add(self, tokens: list[Color]):
+        raise NotImplementedError
+
+    def remove(self, tokens: list[Color]):
+        raise NotImplementedError
+
+    @property
+    def count(self) -> int:
+        raise NotImplementedError
 
 
-@dataclass
-class Card:
-    id: str
-    level: CardLevel
-    points: int
-    color: GemColor
-    cost: dict[GemColor, int]
+class PlayerData(BaseModel):
+    tokens: TokenInventory
 
 
-@dataclass
-class PlayerData:
-    cards_owned: dict[GemColor, list[Card]]
-    cards_reserved: list[Card]
-    tokens: dict[TokenColor, int]
-    nobles: list[Noble]
-
-
-@dataclass
-class GameData:
+class Game(BaseModel):
     round_number: int
-    this_turns_player_index: int
+    turn_number: int
+    current_player_needs: set[PlayerNecessity]
     players: list[PlayerData]
-    decks: dict[CardLevel, list[Card]]
-    trade_rows: dict[
-        CardLevel, tuple[Card | None, Card | None, Card | None, Card | None]
+    tokens: TokenInventory
+
+    def advance_to_next_turn(self) -> None:
+        """
+        CAUTION: invoking this while `self.current_player_needs_to` is not empty will skip the current player's turn.
+        """
+        self.turn_number += 1
+
+        if self.turn_number >= len(self.players):
+            self.round_number += 1
+            self.turn_number = 0
+
+        self.current_player_needs.clear()
+        self.current_player_needs.add(PlayerNecessity.PERFORM_TURN_ACTION)
+        self.current_player_needs = self.current_player_needs.union(
+            self.get_current_player_additional_needs()
+        )
+
+    def get_current_player_additional_needs(self) -> set[PlayerNecessity]:
+        """
+        NOTE: This does NOT add PlayerNecessity.PERFORM_TURN_ACTION to the set of needs returned.
+        """
+        self.current_player_needs.clear()
+
+        # TODO: Check if the player needs to act.
+
+        raise NotImplementedError
+
+
+class ErrorDetail(BaseModel):
+    check_failed: str
+    detail: str
+
+
+ValidationSuccessResponse = tuple[Literal[True], Game]
+ValidationErrorResponse = tuple[Literal[False], list[ErrorDetail]]
+ValidationResponse = ValidationErrorResponse | ValidationSuccessResponse
+
+
+def do_current_player_action_take_tokens(
+    game: Game, tokens_to_take: list[Color]
+) -> ValidationResponse:
+    """
+    Returns a tuple with two values:
+    - If the action succeeded, then the first value is True and the second value is the game's state after the change.
+    - If the action failed, then the first value is False and the second value is a list of reasons why the action failed.
+    """
+    errors = [
+        error_detail
+        for error_detail in [
+            check_current_player_must_take_action(game),
+            check_tokens_are_gem_colors(tokens_to_take),
+            check_no_more_than_3_tokens_taken(tokens_to_take),
+            check_3_tokens_taken_must_be_all_different_colors(tokens_to_take),
+            check_2_tokens_taken_of_same_color_4_must_remain(game, tokens_to_take),
+            check_tokens_are_available(game, tokens_to_take),
+        ]
+        if error_detail is not None
     ]
-    tokens: dict[TokenColor, int]
-    nobles: list[Noble]
 
+    if errors:
+        return False, errors
 
-def validate_action__actor_is_this_turns_player(
-    game: GameData, player_index: int
-) -> list[str]:
-    """
-    Returns a list of the problems with the proposed action.
-    If the list is empty, there are no problems.
-    """
-    errors = []
+    # Perform the action.
+    game_after_action = game.copy()
+    player_after_action = game.players[game.turn_number].copy()
 
-    # The actor must be the player whose turn it is.
-    if game.this_turns_player_index != player_index:
-        errors.append("It is not your turn.")
+    game_after_action.tokens.remove(tokens_to_take)
+    player_after_action.tokens.add(tokens_to_take)
 
-    return errors
+    game_after_action.players[game.turn_number] = player_after_action
 
-
-def validate_action__take_tokens(
-    game: GameData,
-    player_index: int,
-    *,
-    tokens_taken: list[GemColor],
-    tokens_returned: list[TokenColor] = [],
-) -> list[str]:
-    """
-    Returns an exhaustive list of the problems with the proposed Take Tokens action.
-    If the list is empty, there are no problems.
-    """
-    # Check that it's your turn.
-    errors = validate_action__actor_is_this_turns_player(game, player_index)
-
-    # Check rule violations.
-    colors_of_tokens_taken = set(tokens_taken)
-
-    taking_more_than_3_tokens = len(tokens_taken) > 3
-
-    taking_3_with_more_than_one_of_a_color = (
-        len(tokens_taken) == 3 and len(colors_of_tokens_taken) != 3
-    )
-
-    taking_2_of_same_color_when_fewer_than_4_exist = (
-        len(tokens_taken) == 2
-        and len(colors_of_tokens_taken) == 1
-        and len(game.tokens[tokens_taken[0]]) < 4
-    )
-
-    if taking_more_than_3_tokens:
-        errors.append("You may not take more than three tokens.")
-    elif taking_3_with_more_than_one_of_a_color:
-        errors.append("You must take all different colors when taking three tokens.")
-    elif taking_2_of_same_color_when_fewer_than_4_exist:
-        errors.append(
-            "You may not take two tokens of the same color if fewer than four are available."
+    # Indicate whether or not the player now needs to discard tokens.
+    if player_after_action.tokens.count > 10:
+        game_after_action.current_player_needs.add(
+            PlayerNecessity.DISCARD_EXCESS_TOKENS
         )
 
-    # Check game inventory.
-    for color in colors_of_tokens_taken:
-        if game.tokens[color] == 0:
-            errors.append(f"There are no {color} tokens available.")
+    # Indicate that the player has acted.
+    game_after_action.current_player_needs.discard(PlayerNecessity.PERFORM_TURN_ACTION)
 
-    # Check tokens returned.
-    if len(tokens_returned) > len(tokens_taken):
-        errors.append("You may not return more tokens than you have taken.")
+    # If nothing remains to be done, advance to the next turn.
+    if all(
+        possible_need not in game_after_action.current_player_needs
+        for possible_need in [
+            PlayerNecessity.DISCARD_EXCESS_TOKENS,
+            PlayerNecessity.SELECT_A_NOBLE,
+        ]
+    ):
+        game_after_action.advance_to_next_turn()
 
-    combined_token_counts = game.players[player_index].tokens.copy()
-
-    for color in tokens_taken:
-        combined_token_counts[color] += 1
-
-    for color in tokens_returned:
-        combined_token_counts[color] -= 1
-
-    for color, count in combined_token_counts.items():
-        if count < 0:
-            errors.append(f"You don't have enough {color} tokens to return that many.")
-
-    # Check token count at end of round.
-    total_token_count_after_action = sum(combined_token_counts.values())
-
-    if total_token_count_after_action > 10:
-        errors.append(
-            f"You must put back {total_token_count_after_action - 10} more tokens to bring your total down to 10."
-        )
-
-    return errors
+    return True, game_after_action
 
 
-def validate_action__purchase_card(
-    game: GameData,
-    player_index: int,
-    *,
-    card_location: CardLevel | Literal["reserve"],
-    card_index: int,
-    tokens_returned: list[GemColor] = [],
-) -> list[str]:
-    """
-    Returns a list of the problems with the proposed Purchase Card action.
-    If the list is empty, there are no problems.
-    """
-    errors = validate_action__actor_is_this_turns_player(game, player_index)
-
-    player = game.players[player_index]
-
-    # Check that the card is available.
-    card_to_buy = None
-
-    # Check the player's reserve for the card.
-    if card_location == "reserve":
-        if card_index not in range(3):
-            errors.append(
-                f"Position {card_index} is out of bounds for a reserved card."
-            )
-        elif len(player.cards_reserved) <= card_index:
-            errors.append(f"You do not have a reserved card at position {card_index}.")
-        else:
-            card_to_buy = player.cards_reserved[card_index]
-
-    # Check the trade rows for the card.
-    elif card_location in CardLevel:
-        if card_index not in range(4):
-            errors.append(
-                f"Position {card_index} is out of bounds for a card in the trade row."
-            )
-        elif game.trade_rows[card_location] is None:
-            errors.append(
-                f"There is no card at position {card_index} in the trade row at level {card_location}."
-            )
-        else:
-            card_to_buy = game.trade_rows[card_location][card_index]
-
-    # There are no other places the card can be.
-    else:
-        errors.append(
-            f'{card_location} is not a valid location wherein cards may be purchased. Valid locations are "reserve", 1, 2, or 3.'
-        )
-
-    if card_to_buy is None:
-        return errors
-
-    # Now that the card is located, check that the player can pay for it.
-    gems_on_cards_owned = {
-        gem_color: len(card_list) for gem_color, card_list in player.cards_owned.items()
-    }
-
-    tokens_needed_to_buy_card = card_to_buy.cost.copy()
-
-    for color, count in gems_on_cards_owned.items():
-        tokens_needed_to_buy_card[color] -= count
-
-    tokens_needed_to_buy_card = {
-        color: count for color, count in tokens_needed_to_buy_card.items() if count > 0
-    }
-
-    # TODO: Compare the token cost of the card against the player's inventory.
-
-    return errors
+def check_current_player_must_take_action(game: Game) -> ErrorDetail | None:
+    raise NotImplementedError
 
 
-def validate_action__reserve_card(
-    game: GameData,
-    player_index: int,
-    *,
-    card_id: str | None = None,
-    top_of_deck: CardLevel | None = None,
-    tokens_returned: list[TokenColor] = [],
-) -> list[str]:
-    """
-    Returns a list of the problems with the proposed action.
-    If the list is empty, there are no problems.
-    """
-    errors = validate_action__actor_is_this_turns_player(game, player_index)
+def check_no_more_than_3_tokens_taken(
+    tokens_to_take: list[Color],
+) -> ErrorDetail | None:
+    raise NotImplementedError
 
-    # Check that the player can pay for the card.
 
-    return errors
+def check_tokens_are_gem_colors(tokens_to_take: list[Color]) -> ErrorDetail | None:
+    raise NotImplementedError
+
+
+def check_3_tokens_taken_must_be_all_different_colors(
+    tokens_to_take: list[Color],
+) -> ErrorDetail | None:
+    raise NotImplementedError
+
+
+def check_2_tokens_taken_of_same_color_4_must_remain(
+    game: Game,
+    tokens_to_take: list[Color],
+) -> ErrorDetail | None:
+    raise NotImplementedError
+
+
+def check_tokens_are_available(
+    game: Game, tokens_to_take: list[Color]
+) -> ErrorDetail | None:
+    raise NotImplementedError
